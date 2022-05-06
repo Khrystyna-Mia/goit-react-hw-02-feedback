7acb12d96dd9b4a63169d753536f1dff2c0f8752
Khrystyna Hrekul <88985351+Khrystyna-Mia@users.noreply.github.com>
2022-05-07 02:38:17 +0300

    add (styled-components)

---
+++ src/components/Notification/Notification.styled.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Title = styled.p`
+  font-family: 'Georgia', sans-serif;
+  font-weight: 600;
+  font-size: 20px;
+  line-height: 1.8;
+  letter-spacing: 0.2em;
+`;
+
+export { Title };
